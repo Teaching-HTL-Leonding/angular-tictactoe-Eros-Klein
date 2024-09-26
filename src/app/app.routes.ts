ffae07c57e0d7ff4afd06242d70b077c2fce7397
eros-klein <e.klein@HTBLALeonding.onmicrosoft.com>
2024-09-26 19:57:46 +0200

    added necessary routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {TicTacToeComponent} from "./tic-tac-toe/tic-tac-toe.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {"path":"", "redirectTo":"tic-tac-toe", "pathMatch":"full"},
+  {"path":"tic-tac-toe", "component":TicTacToeComponent}
+];
